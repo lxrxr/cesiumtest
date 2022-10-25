fbe8729b590dbe665182bb48a6c78255677fcce9
lxrxr <33959875+lxrxr@users.noreply.github.com>
2022-10-25 20:14:58 +0800

    Initial commit

---
+++ src/views/CesiumContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script>
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+import { onMounted } from 'vue'
+import { Viewer } from 'cesium' // OR  /* import * as Cesium from 'cesium' */
+
+export default {
+  name: 'CesiumContainer',
+  setup() {
+    onMounted(() => {
+      /* eslint no-new: */
+      new Viewer('cesiumContainer')
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
